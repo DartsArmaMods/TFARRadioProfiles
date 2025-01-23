c218972839d50e902f5c35b0729173f4cef45d84
DartRuffian <skulldude327@gmail.com>
2025-01-23 12:43:22 -0600

    Redo the UI spacing

--- addons/profiles/gui.hpp
+++ addons/profiles/gui.hpp
@@ -9,87 +9,77 @@ class GVAR(display) {
     onLoad = QUOTE(call FUNC(onDisplayOpen));
     onUnload = QUOTE(call FUNC(onDisplayClose));
 
+    movingEnable = 1;
+
     class controls {
+        class textTitle: RscText {
+            idc = -1;
+            moving = 1;
+            text = CSTRING(radioProfiles);
+            x = QUOTE(CENTER_X);
+            y = QUOTE(safeZoneY + (5 * GRID_H));
+            w = QUOTE(WIDTH_TOTAL);
+            h = QUOTE((TEXT_SIZE + 2) * GRID_H);
+            sizeEx = QUOTE((TEXT_SIZE + 2) * GRID_H);
+            colorBackground[] = {GUI_BCG_RGB, 1};
+        };
+
         class centerBox: ctrlControlsGroupNoScrollbars {
             idc = -1;
-            x = QUOTE(0.5 - GRID_W * (DISPLAY_WIDTH / 2));
-            y = QUOTE(0.9 - GRID_H * (DISPLAY_HEIGHT / 2));
-            w = QUOTE(GRID_W * DISPLAY_WIDTH);
-            h = QUOTE(GRID_H * DISPLAY_HEIGHT);
+            x = QUOTE(CENTER_X);
+            y = QUOTE(safeZoneY + (5 * GRID_H));
+            w = QUOTE(WIDTH_TOTAL);
+            h = QUOTE(HEIGHT_TOTAL + (100 * GRID_H));
+
             class controls {
-                class centerBackground: ctrlStaticBackground {
-                    idc = -1;
-                    x = QUOTE(0);
-                    y = QUOTE(0);
-                    w = QUOTE(80 * GRID_W);
-                    h = QUOTE(safeZoneH - (45 * GRID_H));
-                    colorBackground[] = {0.13, 0.13, 0.13, 0.9};
-                };
-                class centerTitle: ctrlStaticTitle {
-                    idc = -1;
-                    style = ST_CENTER;
-                    text = "";
-                    x = QUOTE(0);
-                    y = QUOTE(0);
-                    w = QUOTE(80 * GRID_W);
-                    h = QUOTE(5 * GRID_H);
-                    sizeEx = QUOTE(5 * GRID_H);
-                };
-                class textTitle: RscText {
-                    idc = -1;
-                    text = CSTRING(radioProfiles);
-                    x = QUOTE(0);
-                    y = QUOTE(0);
-                    w = QUOTE(80 * GRID_W);
-                    h = QUOTE(5 * GRID_H);
-                    sizeEx = QUOTE(5 * GRID_H);
-                    colorBackground[] = {0, 0, 0, 0};
-                };
                 class contentPanel: RscListBox {
                     idc = IDC_CONTENT_PANEL;
-                    columns[] = {0, 0.05, 0.40, 0.50, 0.60, 0.70, 0.75, 0.80, 0.85, 0.90};
                     drawSideArrows = 0;
                     disableOverflow = 1;
-                    x = QUOTE(0);
-                    y = QUOTE(5 * GRID_H);
-                    w = QUOTE(160 * GRID_W);
-                    h = QUOTE(safeZoneH - (57 * GRID_H));
-                    sizeEx = QUOTE(3 * GRID_H);
-                };
-                class profileNameEditBox: ctrlEdit {
-                    idc = IDC_PROFILE_NAME_EDIT_BOX;
-                    x = QUOTE(0);
-                    y = QUOTE(safeZoneH - (48 * GRID_H));
-                    w = QUOTE(58 * GRID_W);
-                    h = QUOTE(3 * GRID_H);
-                    sizeEx = QUOTE(3 * GRID_H);
+                    x = 0;
+                    y = QUOTE((TEXT_SIZE + 2) * GRID_H); // Needs to appear under the title box
+                    w = QUOTE(WIDTH_TOTAL);
+                    h = QUOTE(HEIGHT_TOTAL);
+                    colorBackground[] = {0.13, 0.13, 0.13, 0.9};
+                    sizeEx = QUOTE(TEXT_SIZE * GRID_H);
                 };
                 class buttonClose: ctrlButton {
                     idc = -1;
-                    x = QUOTE(75 * GRID_W);
-                    y = QUOTE(0 * GRID_H);
-                    w = QUOTE(5 * GRID_W);
-                    h = QUOTE(5 * GRID_H);
+                    x = QUOTE(WIDTH_TOTAL - ((TEXT_SIZE + 2) * GRID_W));
+                    y = 0;
+                    w = QUOTE((TEXT_SIZE + 2) * GRID_W);
+                    h = QUOTE((TEXT_SIZE + 2) * GRID_H);
+                    sizeEx = QUOTE((TEXT_SIZE + 2) * GRID_H);
                     text = "X";
-                    sizeEx = QUOTE(5 * GRID_H);
+
                     onButtonClick = QUOTE(closeDialog 0);
                     colorText[] = {1, 1, 1, 1};
                     colorBackground[] = {0, 0, 0, 0.2};
                     colorBackgroundActive[] = {0.5, 0, 0, 1};
                 };
 
-                #define BUTTON_WIDTH 13
-                #define BUTTON_SPACING(number) (((BUTTON_WIDTH + 2) * number) * GRID_W)
+                #define BUTTON_WIDTH 40
+                #define BUTTON_SPACING(number) (((BUTTON_WIDTH + 10) * number) * GRID_W)
+
+                class profileNameEditBox: ctrlEdit {
+                    idc = IDC_PROFILE_NAME_EDIT_BOX;
+                    x = 0;
+                    y = QUOTE(HEIGHT_TOTAL + ((TEXT_SIZE + 3) * GRID_H));
+                    w = QUOTE(BUTTON_SPACING(3) + (BUTTON_WIDTH * GRID_W));
+                    h = QUOTE(TEXT_SIZE * GRID_H);
+                    sizeEx = QUOTE(TEXT_SIZE * GRID_H);
+                };
 
                 class buttonSave: ctrlButton {
                     idc = IDC_BUTTON_SAVE;
-                    x = QUOTE(0 * GRID_W);
-                    y = QUOTE(safeZoneH - (44 * GRID_H));
+                    x = 0;
+                    y = QUOTE(HEIGHT_TOTAL + (((TEXT_SIZE * 2) + 5) * GRID_H));
                     w = QUOTE(BUTTON_WIDTH * GRID_W);
-                    h = QUOTE(5 * GRID_H);
+                    h = QUOTE((TEXT_SIZE + 2) * GRID_H);
+
                     text = CSTRING(buttonSaveText);
                     // tooltip = CSTRING(buttonSaveTooltip);
-                    sizeEx = QUOTE(3 * GRID_H);
+                    sizeEx = QUOTE(TEXT_SIZE * GRID_H);
                     onButtonClick = QUOTE([ARR_2(ctrlParent (_this select 0),_this select 0)] call FUNC(buttonSaveProfile));
                     colorBackground[] = {0, 0, 0, 0.8};
                 };
@@ -125,11 +115,11 @@ class GVAR(display) {
             shadow = 0;
             colorBackground[] = {0, 0, 0, 0.7};
             text = "";
-            x = QUOTE(0.5 - GRID_W * (DISPLAY_WIDTH / 2));
-            y = QUOTE(1.8 -GRID_H * (DISPLAY_HEIGHT / 2));
-            w = QUOTE(GRID_W * DISPLAY_WIDTH);
-            h = QUOTE(5 * GRID_H);
-            sizeEx = QUOTE(2 * GRID_H);
+            x = QUOTE(CENTER_X);
+            y = QUOTE(safeZoneH + safeZoneY - 25 * GRID_H);
+            w = QUOTE(WIDTH_TOTAL);
+            h = QUOTE(10 * GRID_H);
+            sizeEx = QUOTE(TEXT_SIZE * GRID_H);
         };
     };
     class controlsBackground {};
